2ca1ea3dd0e5425cab4723b053d8ee65eb48327c
Dinar Akchurin <akchurinda@modeltech.ru>
2020-06-10 22:50:06 +0500

    Добавлена функция возвращающая путь до файла, который необходимо сохранить

--- gui/utils/fp.py
+++ gui/utils/fp.py
@@ -1,6 +1,9 @@
 # coding: utf-8
 
 from pathlib import Path
+from typing import Optional
+
+from PyQt5.QtWidgets import QWidget, QFileDialog
 
 
 def image_filepath_valid(path: Path) -> bool:
@@ -9,3 +12,20 @@ def image_filepath_valid(path: Path) -> bool:
         and path.is_file()
         and path.suffix in {".png", ".jpg", ".jpeg"}
     )
+
+
+def save_file_dialog(
+    parent: QWidget,
+    caption: str,
+    filters: str = "",
+    filepath: str = "",
+    options: Optional[QFileDialog.Options] = None,
+) -> Path:
+    if options is None:
+        options = QFileDialog.Options()
+
+    # noinspection PyCallByClass
+    filepath, _ = QFileDialog.getSaveFileName(
+        parent, caption, filepath, filters, options=options
+    )
+    return Path(filepath)
