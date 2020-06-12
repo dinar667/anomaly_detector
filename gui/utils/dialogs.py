6f57bbd4cf2ceb98ad03fcbe7bdbf7a09c78c4e4
Dinar Akchurin <akchurinda@modeltech.ru>
2020-06-12 14:16:12 +0500

    Вынесена функция создания диалога

--- gui/utils/dialogs.py
+++ gui/utils/dialogs.py
@@ -3,18 +3,21 @@
 from PyQt5.QtWidgets import QWidget, QMessageBox
 
 
-def get_error_message(parent: QWidget) -> QMessageBox:
+def _create_message(parent: QWidget, icon, window_title) -> QMessageBox:
     box = QMessageBox(parent)
-    box.setIcon(QMessageBox.Warning)
-    box.setWindowTitle("Ошибка")
+    box.setIcon(icon)
+    box.setWindowTitle(window_title)
     return box
+
+
+def get_error_message(parent: QWidget) -> QMessageBox:
+    message = _create_message(parent, QMessageBox.Warning, "Ошибка")
+    return message
 
 
 def get_success_message(parent: QWidget) -> QMessageBox:
-    box = QMessageBox(parent)
-    box.setIcon(QMessageBox.Information)
-    box.setWindowTitle("Успех")
-    return box
+    message = _create_message(parent, QMessageBox.Information, "Успех")
+    return message
 
 
 def show_error_dialog(parent: QWidget, text: str) -> None:
